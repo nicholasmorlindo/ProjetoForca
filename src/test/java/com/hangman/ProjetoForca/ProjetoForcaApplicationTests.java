00b3445959945f0da98e9f7c1eba1cc1e1bcbc07
nicholaspassos <nicholasmorlin@ufu.br>
2021-10-17 14:15:29 -0300

    inicializando o projeto

---
+++ src/test/java/com/hangman/ProjetoForca/ProjetoForcaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hangman.ProjetoForca;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoForcaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
